aebf8237ba74936c66e0296485d2d5ee310fe9ee
VictorAkan <vikurekong123@gmail.com>
2022-12-03 13:12:37 -0500

    changed title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,7 @@ export const Header = () => {
     return (
         <div className="flex justify-center">
             <div className="heading text-white text-center mt-7">
-                <h2 className="text-xl">iPhone 14 Pro</h2>
-                <h2 className="font-bold text-2xl">Super. Ultra. Pro.</h2>
+                <h2 className="text-xl font-bold text-2xl">Amazon: Appel manqué Vous avez gagné l'iPhone 14 Pro</h2>
             </div>
         </div>
     )
